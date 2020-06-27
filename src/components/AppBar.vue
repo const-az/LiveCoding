8beea2849784fad018e991728e42afb632d3b211
Constanza Avilés Zavando <constanzaavileszavando@MacBook-Pro-de-Constanza.local>
2020-06-27 11:26:58 -0400

    cloud functions done

---
+++ src/components/AppBar.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <v-app-bar
+      color="deep-purple accent-4"
+      dense
+      dark
+      flat
+    >
+      <v-toolbar-title>LiveCoding</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+    </v-app-bar>
+  </div>
+</template>
--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,40 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
 Vue.use(Vuex)
 
+// Base URL for Firebase cloudfunctions
+const baseURL = 'https://us-central1-ensayoprueba01-eb6f1.cloudfunctions.net/courses'
+
 export default new Vuex.Store({
   state: {
+    courses: [],
+    loading: false
   },
   mutations: {
+    // Toggles loading state
+    SHOW_LOADING(state){ state.loading = true },
+    HIDE_LOADING(state){ state.loading = false },
+    // Sets courses into an state array
+    GET_COURSES(state, courses){
+      state.courses = courses
+    },
   },
   actions: {
+    // CRUD: interacting with Firebase
+    // Gets all courses from Firebase
+    getCourses({commit}){
+      // Displays loading spinner while getting items
+      commit('SHOW_LOADING')
+      axios.get(`${baseURL}/courses`, { headers: { "Content-type": "text/plain"}})
+      .then((accept) => {
+        // Saves info into state and hide spinner
+        let data = accept.data
+        commit('GET_COURSES', data)
+        commit('HIDE_LOADING')
+      })
+    },
   },
   modules: {
   }
